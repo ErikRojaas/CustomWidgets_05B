1a5167e15c923b66092f23ac5be49ab776cce23a
Komart21 <152431488+Komart21@users.noreply.github.com>
2025-01-10 17:10:20 +0100

    demo

--- custom_widgets/macos/Flutter/GeneratedPluginRegistrant.swift
+++ custom_widgets/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import screen_retriever_macos
+import screen_retriever
+import url_launcher_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
